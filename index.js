ebcd182e2634f269e9e034556dc0323bc2de7a04
unknown <ae479730@gmail.com>
2026-07-30 03:09:53 +0300

    feat: add vehicle CRUD operations

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const vehicleRoutes = require("./routes/vehiclesRoutes");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use("/vehicles", vehicleRoutes);
 const MONGO_URI =
-  "mongodb://osamamatter390_db_user:QxDg4ur0gSSAGf5b@ac-1kq2nt9-shard-00-00.sinmsue.mongodb.net:27017,ac-1kq2nt9-shard-00-01.sinmsue.mongodb.net:27017,ac-1kq2nt9-shard-00-02.sinmsue.mongodb.net:27017/TransportationProject?ssl=true&replicaSet=atlas-6zxqi3-shard-0&authSource=admin";
+  "mongodb://ahmedemad:MCCNMVcBnFH28uq0@ac-1kq2nt9-shard-00-00.sinmsue.mongodb.net:27017,ac-1kq2nt9-shard-00-01.sinmsue.mongodb.net:27017,ac-1kq2nt9-shard-00-02.sinmsue.mongodb.net:27017/TransportationProject?ssl=true&replicaSet=atlas-6zxqi3-shard-0&authSource=admin";
 
 mongoose
   .connect(MONGO_URI)
